858788032adb2eebcf6011f7259005259dd0748f
pydude <45510606+MySecondLanguage@users.noreply.github.com>
2024-06-20 16:11:26 +0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+"""
+plugin_name: stripe
+plugin_uri: https://github.com/nxtbn-com/stripe
+version: 1.0.1
+author: bytenyx limited
+author_uri: http://bytenyx.com
+description: plugin to handle payment via stripe.
+license: BSD-3-Clause
+nxtbn_version_compatibility: >=1.0.0
+"""
+
 from nxtbn.payment.plugins.stripe.stripe_gateway import StripePaymentGateway
 
 gateway = StripePaymentGateway
